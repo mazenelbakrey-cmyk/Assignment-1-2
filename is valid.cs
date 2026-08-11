283f52f44ca58a2ed6e9f09ebf3a811c79ffa70b
Mazen Elbakrey <mazenelbakrey@gmail.com>
2026-08-11 15:03:52 +0300

    Update is valid.cs

--- is valid.cs
+++ is valid.cs
@@ -1,7 +1,7 @@
 ﻿Console.WriteLine("Enter the number : ");
 int number = int.Parse(Console.ReadLine());
 
-if (number>1 && number<10)
+if (number>=1 && number<=10)
 {
     Console.WriteLine("Valid");
 }
